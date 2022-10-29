188ed43a0da9250603389416ffb64fb5d81637f5
Kay Bi <kaybi9x@gmail.com>
2022-10-29 15:37:12 +0700

    test

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express();
+const port = process.env.PORT
+
+app.get('/', (req, res)=> res.send('OK'))
+
+app.listen(port, ()=> console.log(`Server is running at http://localhost:${port}`))
